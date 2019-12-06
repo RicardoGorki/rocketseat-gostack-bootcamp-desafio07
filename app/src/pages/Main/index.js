a260fd9598970bf27165c18a772ceb755c42d381
RicardoGorki <ricardo.gorki@gmail.com>
2019-12-06 17:16:44 -0300

    initial

---
+++ app/src/pages/Main/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import { Container, View } from './styles';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <Container>
+        <View />
+      </Container>
+    );
+  }
+}
